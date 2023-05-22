4761030c39884690330e9ddf18e247c5c0408dd4
dgallula <dgallua@gmail.com>
2023-05-22 16:43:26 +0200

    Models controler ok

--- tutoLaravel/app/Http/Controllers/BlogController.php
+++ tutoLaravel/app/Http/Controllers/BlogController.php
@@ -2,23 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\BlogFilterRequest;
 use App\Models\Post;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Validation\Rule;
 use Illuminate\View\View;
+use Illuminate\Support\Facades\Validator;
 
 class BlogController extends Controller
 {
-    public function index(): View{
+    public function index(BlogFilterRequest $request): View{
+
       return view('blog.index', [
           'posts'=>  Post::paginate(2)
 
         ]);
     }
 
-    public function show(string $slug, string $id): RedirectResponse | View
-    {
+    public function show(string $slug, string $id): RedirectResponse |View {
+
+
+
 
         $post= \App\Models\Post::findorFail($id);
         if ($post->slug !== $slug){
